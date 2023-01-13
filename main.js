ac6f0f24a358cf238a906e5bc49f7cff66dad136
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2023-01-12 23:29:48 -0300

    Add style

--- main.js
+++ main.js
@@ -1,17 +1,43 @@
-const fragment = new DocumentFragment();
 const url = 'http://localhost:3000/data';
 
 fetch(url).
   then((response) => response.json()).
   then((data) => {
+    const tbody = document.querySelector('tbody');
+
     data.forEach(function(exam) {
-      const li = document.createElement('li');
-      li.textContent = `${exam.nome_paciente} - ${exam.data_exame} - ${exam.nome_medico} - ${exam.tipo_exame} - ${exam.limites_tipo_exame} - ${exam.resultado_tipo_exame}`;
-      fragment.appendChild(li);
+      const tr = document.createElement('tr');
+
+      const tdNomePaciente = document.createElement('td');
+      tdNomePaciente.textContent = exam.nome_paciente;
+      tr.appendChild(tdNomePaciente);
+
+      const tdNomeMedico = document.createElement('td');
+      tdNomeMedico.textContent = exam.nome_medico;
+      tr.appendChild(tdNomeMedico);
+
+      const tdCrmMedico = document.createElement('td');
+      tdCrmMedico.textContent = exam.crm_medico;
+      tr.appendChild(tdCrmMedico);
+
+      const tdDataExame = document.createElement('td');
+      tdDataExame.textContent = exam.data_exame;
+      tr.appendChild(tdDataExame);
+
+      const tdTipoExame = document.createElement('td');
+      tdTipoExame.textContent = exam.tipo_exame;
+      tr.appendChild(tdTipoExame);
+
+      const tdLimitesTipoExame = document.createElement('td');
+      tdLimitesTipoExame.textContent = exam.limites_tipo_exame;
+      tr.appendChild(tdLimitesTipoExame);
+
+      const tdResultadoTipoExame = document.createElement('td');
+      tdResultadoTipoExame.textContent = exam.resultado_tipo_exame;
+      tr.appendChild(tdResultadoTipoExame);
+
+      tbody.appendChild(tr);
     })
-  }).
-  then(() => {
-    document.querySelector('ul').appendChild(fragment);
   }).
   catch(function(error) {
     console.log(error);
