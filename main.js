fcb1dfd0755e2c5db65d23239757187c2a21683f
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2023-01-18 23:35:29 -0300

    Add patient and physician data into index page

--- main.js
+++ main.js
@@ -23,8 +23,113 @@ document.querySelector('form').addEventListener("submit", function(event) {
         errorMessage.classList.add("hidden");
       }
 
+      const titleToken = document.createElement('h2');
+      titleToken.textContent = "Resultados para o token: " + formatted_data["result_token"];
+      header.appendChild(titleToken);
+
+      const brA = document.createElement('br');
+      header.appendChild(brA);
+
+      const patient = document.createElement('h3');
+      patient.textContent = "Dados do paciente";
+      header.appendChild(patient);
+
+      const strongDate = document.createElement('strong');
+      strongDate.textContent = "Data de realização dos exames: ";
+      header.appendChild(strongDate);
+
+      const testDate = new Date(formatted_data["result_date"]);
+      const testDay = testDate.getDate();
+      const testMonth = testDate.getMonth();
+      const testYear = testDate.getFullYear();
+      const spanDate = document.createElement('span');
+      spanDate.textContent = `${testDay}/${testMonth}/${testYear}`;
+      header.appendChild(spanDate);
+
+      const brB = document.createElement('br');
+      header.appendChild(brB);
+
+      const strongCpf = document.createElement('strong');
+      strongCpf.textContent = "CPF: ";
+      header.appendChild(strongCpf);
+
+      const spanCpf = document.createElement('span');
+      spanCpf.textContent = formatted_data["cpf"];
+      header.appendChild(spanCpf);
+
+      const brC = document.createElement('br');
+      header.appendChild(brC);
+
+      const strongName = document.createElement('strong');
+      strongName.textContent = "Nome: ";
+      header.appendChild(strongName);
+
+      const spanName = document.createElement('span');
+      spanName.textContent = formatted_data["name"];
+      header.appendChild(spanName);
+
+      const brD = document.createElement('br');
+      header.appendChild(brD);
+
+      const strongEmail = document.createElement('strong');
+      strongEmail.textContent = "E-mail: ";
+      header.appendChild(strongEmail);
+
+      const spanEmail = document.createElement('span');
+      spanEmail.textContent = formatted_data["email"];
+      header.appendChild(spanEmail);
+
+      const brE = document.createElement('br');
+      header.appendChild(brE);
+
+      const strongBirthday = document.createElement('strong');
+      strongBirthday.textContent = "Data de nascimento: ";
+      header.appendChild(strongBirthday);
+
+      const date = new Date(formatted_data["birthday"]);
+      const day = date.getDate();
+      const month = date.getMonth();
+      const year = date.getFullYear();
+      const spanBirthday = document.createElement('span');
+      spanBirthday.textContent = `${day}/${month}/${year}`;
+      header.appendChild(spanBirthday);
+
+      const physician = document.createElement('h3');
+      physician.textContent = "Dados do médico";
+      header.appendChild(physician);
+
+      const strongCrm = document.createElement('strong');
+      strongCrm.textContent = "CRM: ";
+      header.appendChild(strongCrm);
+
+      const spanCrm = document.createElement('span');
+      spanCrm.textContent = formatted_data["doctor"]["crm"];
+      header.appendChild(spanCrm);
+
+      const brF = document.createElement('br');
+      header.appendChild(brF);
+
+      const strongCrmState = document.createElement('strong');
+      strongCrmState.textContent = "Estado CRM: ";
+      header.appendChild(strongCrmState);
+
+      const spanCrmState = document.createElement('span');
+      spanCrmState.textContent = formatted_data["doctor"]["crm_state"];
+      header.appendChild(spanCrmState);
+
+      const brG = document.createElement('br');
+      header.appendChild(brG);
+
+      const strongPhysician = document.createElement('strong');
+      strongPhysician.textContent = "Nome: ";
+      header.appendChild(strongPhysician);
+
+      const spanPhysician = document.createElement('span');
+      spanPhysician.textContent = formatted_data["doctor"]["name"];
+      header.appendChild(spanPhysician);
+
       formatted_data.tests.forEach(function(exam) {
-        console.log(exam)
+
         const tr = document.createElement('tr');
 
         const tdType = document.createElement('td');
@@ -40,6 +145,7 @@ document.querySelector('form').addEventListener("submit", function(event) {
         tr.appendChild(tdResult);
 
         tbody.appendChild(tr);
+
       })
     }).
     catch(function(error) {
